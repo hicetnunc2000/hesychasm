e59f3168f4bee17f8b4b4f00589443f9afc73eca
crzypatchwork <180111973@aluno.unb.br>
2021-01-31 18:10:22 -0300

    release

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask
 from flask_restx import Api
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 # ROUTES
 
--- routes/ipfs.py
+++ routes/ipfs.py
@@ -4,6 +4,8 @@ from werkzeug import FileStorage
 from libs.validate import Validate
 
 import ipfshttpclient
+import json
+import os
 
 api = Namespace('ipfs')
 upload_parser = api.parser()
@@ -14,17 +16,20 @@ conn = ipfshttpclient.connect('/dns4/ipfs.infura.io/tcp/5001/https')
 
 v = Validate()
 
+
 @api.route('/post_file')
 @api.expect(upload_parser)
 class post_file(Resource):
     @api.expect(type)
     def post(self):
-        try:
-            args = upload_parser.parse_args()
-            cid = conn.add_bytes(args['file'].read())
-            return {'result': cid}
-        except:
-            return 500
+        with open('/tmp/OBJKT', 'wb') as w:
+            w.write(request.files['file'].read())
+
+        cid = conn.add('/tmp/OBJKT', recursive=True)
+        os.remove('/tmp/OBJKT')
+
+        return { 'result': cid['Hash'] }
+
 
 @api.route('/post_json')
 class post_json(Resource):
@@ -36,15 +41,16 @@ class post_json(Resource):
         except:
             return 500
 
+
 @api.route('/read_cid')
 @api.doc(params={
-    'cid': 'CID hash'
+    'cid': 'IPFS CID hash'
 })
 class read_cid(Resource):
     def post(self):
         try:
             payload = v.read_requests(request)
-            json = conn.get_json(payload['cid'])
-            return {'result': json}
+            j = conn.get_json(payload['cid'])
+            return {'result': j}
         except:
             return 500
